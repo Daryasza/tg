a36fb2771b8e547451418837804eb618fd2d686f
Darya Sharnevich <darya.sharnevich@gmail.com>
2022-11-02 17:37:26 +0300

    make it better

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const packageInfo = require('./package.json');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.json());
+
+app.get('/', function (req, res) {
+  res.json({ version: packageInfo.version });
+});
+
+const server = app.listen(process.env.PORT, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Web server started at http://%s:%s', host, port);
+});
+
+module.exports = function (bot) {
+  app.post('/' + bot.token, function (req, res) {
+    bot.processUpdate(req.body);
+    res.sendStatus(200);
+  });
+};
